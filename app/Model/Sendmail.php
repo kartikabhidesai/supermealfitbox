7399becfd5408136ab9121889c517aa1088126b4
kartikdesai123 <kartikdesai123@gmail.com>
2019-12-18 20:36:21 +0530

    change

--- app/Model/Sendmail.php
+++ app/Model/Sendmail.php
@@ -5,29 +5,30 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 use Config;
 use Auth;
-use DB; 
+use DB;
 use Mail;
 
-class Sendmail extends Model
-{
-    public function sendSMTPMail($mailData){
-        
+class Sendmail extends Model {
+
+    public function sendSMTPMail($mailData) {
+
         $pathToFile = $mailData['attachment'];
-           
-            $mailsend = Mail::send($mailData['template'], ['data' => $mailData['data']], function ($m) use ($mailData,$pathToFile) {
-                 $m->from('kartikdesai123@gmail.com', 'Supermealfitbox');
-      
-                 $m->to($mailData['mailto'], "Supermealfitbox")->subject($mailData['subject']);
-                 if($pathToFile != ""){
-                     // $m->attach($pathToFile);
-                 }
-                 
-                //  $m->cc($mailData['bcc']);
-             });
-             if($mailsend){
-                 return true;
-             }else{
-                 return false;
-             }
+        $frommail = env('MAIL_USERNAME');
+        $mailsend = Mail::send($mailData['template'], ['data' => $mailData['data']], function ($m) use ($mailData, $pathToFile) {
+                    $m->from($frommail, 'Supermealfitbox');
+
+                    $m->to($mailData['mailto'], "Supermealfitbox")->subject($mailData['subject']);
+                    if ($pathToFile != "") {
+                        // $m->attach($pathToFile);
+                    }
+
+                    //  $m->cc($mailData['bcc']);
+                });
+        if ($mailsend) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
 }
