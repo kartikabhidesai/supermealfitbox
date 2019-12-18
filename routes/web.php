5b4bfe0e8429e81ec6c1f1952aaa3c63a7657ddb
kartikdesai123 <kartikdesai123@gmail.com>
2019-12-18 20:20:24 +0530

    change

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,23 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
+Route::get('clear', function() {
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode1 = Artisan::call('route:clear');
+    $exitCode2 = Artisan::call('config:clear');
+    $exitCode3 = Artisan::call('view:clear');
+    return '<h1>cache route config view cleared</h1>';
+});
+
 //
 Route::get('/', function () {
     return view('frontend/pages/dashboard');
